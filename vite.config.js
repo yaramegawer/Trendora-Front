94bbd33483a453eb5448b06f1c92309364415ca2
Yara Sayed <yara.yarasayed@gmail.com>
2025-09-29 09:18:13 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://trendora-nine.vercel.app/',
         changeOrigin: true,
         secure: false,
       }
