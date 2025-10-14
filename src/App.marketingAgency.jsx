f4b5459b54dabb005f15cd5a7908532ea909a37b
Yara Sayed <yara.yarasayed@gmail.com>
2025-10-14 10:51:29 +0300

    final touches

--- src/App.marketingAgency.jsx
+++ src/App.marketingAgency.jsx
@@ -89,14 +89,6 @@ const App = () => {
   );
 };
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(reg => console.log('✅ Service Worker registered:', reg))
-      .catch(err => console.log('❌ Service Worker registration failed:', err));
-  });
-}
 
 
 export default App;
